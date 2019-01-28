c920d020da1376710e9c956b6d48e43ac0b35ff6
Bob Clary <bob@bclary.com>
2019-01-27 16:35:01 -0800

    Fix installation path in bin/start_android_hardware_testing.sh.

--- bin/start_android_hardware_testing.sh
+++ bin/start_android_hardware_testing.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 . /etc/bitbar/bitbar.env
-. /home/bitbar/venv/bin/activate
-export PYTHONPATH=/home/bitbar
-python /home/bitbar/mozilla_bitbar_devicepool/main.py start-test-run-manager --delete-bitbar-tests
+. /home/bitbar/mozilla-bitbar-devicepool/venv/bin/activate
+export PYTHONPATH=/home/bitbar/mozilla-bitbar-devicepool
+python /home/bitbar/mozilla-bitbar-devicepool/mozilla_bitbar_devicepool/main.py start-test-run-manager --delete-bitbar-tests
 
