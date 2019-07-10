eb532f80597652bca0ffe996dad77b09a186f585
Andrew Erickson <aerickson@mozilla.com>
2019-07-10 10:35:49 -0700

    start script: add  --update-bitbar

--- bin/start_android_hardware_testing.sh
+++ bin/start_android_hardware_testing.sh
@@ -3,5 +3,5 @@
 . /etc/bitbar/bitbar.env
 . /home/bitbar/mozilla-bitbar-devicepool/venv/bin/activate
 export PYTHONPATH=/home/bitbar/mozilla-bitbar-devicepool
-python /home/bitbar/mozilla-bitbar-devicepool/mozilla_bitbar_devicepool/main.py start-test-run-manager --delete-bitbar-tests
+python /home/bitbar/mozilla-bitbar-devicepool/mozilla_bitbar_devicepool/main.py start-test-run-manager --delete-bitbar-tests --update-bitbar
 
