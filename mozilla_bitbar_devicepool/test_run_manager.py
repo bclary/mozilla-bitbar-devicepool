871e4e4ad9b0aeff9e2f74e184f5cf8ee652d383
Andrew Erickson <aerickson@mozilla.com>
2019-08-16 10:58:31 -0700

    tweak section mentioned by bc

--- mozilla_bitbar_devicepool/test_run_manager.py
+++ mozilla_bitbar_devicepool/test_run_manager.py
@@ -60,10 +60,7 @@ class TestRunManager(object):
         device_group_count = bitbar_device_group['deviceCount']
 
         offline_devices = []
-        if 'device_model' in project_config:
-            temp_offline_devices = get_offline_devices(device_model=project_config['device_model'])
-        else:
-            temp_offline_devices = get_offline_devices()
+        temp_offline_devices = get_offline_devices(device_model=project_config.get('device_model', None))
         for device_name in temp_offline_devices:
             if device_name in device_group:
                 offline_devices.append(device_name)
