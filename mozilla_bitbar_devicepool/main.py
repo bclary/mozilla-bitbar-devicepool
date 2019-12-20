424f2fc4f78a9e0a3a16feb7c7aac75c400956a4
Andrew Erickson <aerickson@mozilla.com>
2019-12-20 13:04:53 -0600

    fix typo

--- mozilla_bitbar_devicepool/main.py
+++ mozilla_bitbar_devicepool/main.py
@@ -69,7 +69,6 @@ def run_test(args):
         bitbar_configpath = args.bitbar_config
 
     configuration.configure(bitbar_configpath, filespath=args.files, update_bitbar=args.update_bitbar)
-s
     run_test_for_project(args.project_name)
     logger.info("run started for project '%s'" % args.project_name)
 
