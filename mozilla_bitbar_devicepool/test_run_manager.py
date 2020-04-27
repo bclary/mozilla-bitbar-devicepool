046669027b4dfabd7bb856b0971629f2d07bf778
Andrew Erickson <aerickson@mozilla.com>
2020-04-27 16:24:03 -0700

    don't set TESTING=True

--- mozilla_bitbar_devicepool/test_run_manager.py
+++ mozilla_bitbar_devicepool/test_run_manager.py
@@ -23,7 +23,7 @@ from testdroid import RequestResponseError
 # WARNING: not used everywhere yet!!!
 #
 # don't fire calls at bitbar, just mention you would
-TESTING = True
+TESTING = False
 
 CACHE = None
 CONFIG = None
