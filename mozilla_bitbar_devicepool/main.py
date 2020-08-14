cf939e775c6c623ec09d9d1d580f31c807037c83
Andrew Erickson <aerickson@mozilla.com>
2020-08-14 14:47:26 -0700

    optimize imports

--- mozilla_bitbar_devicepool/main.py
+++ mozilla_bitbar_devicepool/main.py
@@ -13,10 +13,9 @@ from mozilla_bitbar_devicepool import (
     logger,
     modulepath,
 )
-from mozilla_bitbar_devicepool.util.network import download_file
 from mozilla_bitbar_devicepool.bitbar.runs import run_test_for_project
 from mozilla_bitbar_devicepool.test_run_manager import TestRunManager
-
+from mozilla_bitbar_devicepool.util.network import download_file
 
 testdroid_apk_url = (
     "https://github.com/bitbar/bitbar-samples/blob/master/apps/builds/Testdroid.apk"
