7d451e8d53ccd57f30bcaa9b6e540ab834f8f7a4
Andrew Erickson <aerickson@mozilla.com>
2020-10-07 13:45:42 -0700

    process_active_runs: need to remove user id

--- mozilla_bitbar_devicepool/test_run_manager.py
+++ mozilla_bitbar_devicepool/test_run_manager.py
@@ -226,7 +226,10 @@ class TestRunManager(object):
             return
 
         for item in result:
-            project_name = item["projectName"]
+            # remove user id from this (see configuration.py:configure_projects)
+            project_name = item["projectName"].replace(
+                "%s-" % (CACHE["me"]["mainUserId"])
+            )
             # only accumulate for projects in our config
             if project_name in bitbar_projects:
                 accumulation_dict[project_name].append(item)
